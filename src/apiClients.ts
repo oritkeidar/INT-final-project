b873d0f6fea37faf817455e5e419dc8c8e0d2d1b
oritkeidar <113340998+oritkeidar@users.noreply.github.com>
2023-10-29 14:17:59 +0200

    Delete src directory

--- src/apiClients.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const axiosClient = axios.create(); // we have created specific instance of axios so we can define on it middlewears (interceptors)
-
-export default axiosClient;
